042a5b6d524f2fb417e461503968089f69aff9d4
pablocenteno <pablo.centeno2@educa.madrid.org>
2023-02-17 12:30:45 +0100

    otro

--- src/main/java/aad/pablo/controlador/PlanControlador.java
+++ src/main/java/aad/pablo/controlador/PlanControlador.java
@@ -1,6 +1,7 @@
 package aad.pablo.controlador;
 
 
+import aad.pablo.modelo.Plan;
 import aad.pablo.servicio.PlanServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,9 +28,30 @@ public class PlanControlador {
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<String>obtenerPlan(@RequestParam Long idPlan){
         return ResponseEntity.status(HttpStatus.OK)
-                .header()
-                .body();
+                .header("Content-Type","application/json")
+                .body(toJson(planServicio.obtenerPlan(idPlan)));
     }
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<String>crearPlan(@RequestParam Plan plan){
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-Type","application/json")
+                .body(toJson(planServicio.crearPlan(plan)));
+    }
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<String>modificarPlan(@RequestParam Plan plan){
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-Type","application/json")
+                .body(toJson(planServicio.modificarPlan(plan)));
+    }
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<Boolean>borrarPLan(@RequestParam Long  idPlan){
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-Type","application/json")
+                .body(planServicio.borrarPlan(idPlan));
+    }
+
+
+
 
 
 }
--- src/main/java/aad/pablo/controlador/UsuarioControlador.java
+++ src/main/java/aad/pablo/controlador/UsuarioControlador.java
@@ -1,8 +1,42 @@
 package aad.pablo.controlador;
 
 
+import aad.pablo.modelo.Usuario;
+import aad.pablo.servicio.PlanServicio;
+import aad.pablo.servicio.UsuarioServicio;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UsuarioControlador {
+
+    public String toJson(Object object)
+    {
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+
+        return gson.toJson(object);
+    }
+    @Autowired
+    UsuarioServicio usuarioServicio;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<String> obtenerPlan(@RequestParam Long idPlan){
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-Type","application/json")
+                .body(toJson(usuarioServicio.obtenerPlan(idPlan)));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<String>crearUsuario(@RequestParam Usuario usuario){
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-Type","application/json")
+                .body(toJson(usuarioServicio.crearUsuario(usuario)));
+    }
 }
